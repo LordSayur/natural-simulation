f69c1c6bf31a5d16c8c2b3f7f8c5a6e268c14fb6
Lord Sayur <umar_317@hotmail.com>
2020-09-05 13:54:25 +0800

    Fix navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -12,29 +12,33 @@
       <RandomWalk msg="Natural Simulation" />
     </v-main>
 
-    <v-navigation-drawer v-model="drawer" temporary>
-      <v-list nav dense>
-        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
-          <v-list-item>
-            <v-list-item-title>Foo</v-list-item-title>
-          </v-list-item>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
 
-          <v-list-item>
-            <v-list-item-title>Bar</v-list-item-title>
-          </v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
 
-          <v-list-item>
-            <v-list-item-title>Fizz</v-list-item-title>
-          </v-list-item>
+      <v-divider></v-divider>
 
-          <v-list-item>
-            <v-list-item-title>Buzz</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <v-bottom-navigation :value="activeBtn" grow color="teal" class="d-lg-none">
+    <v-bottom-navigation :value="activeBtn" grow color="teal" class="d-lg-none" fixed>
       <v-btn @click.stop="drawer = !drawer">
         <span>Menu</span>
         <v-icon>mdi-menu</v-icon>
@@ -67,6 +71,10 @@ export default {
     drawer: false,
     group: null,
     activeBtn: 1,
+    items: [
+      { title: "Home", icon: "dashboard" },
+      { title: "About", icon: "question_answer" },
+    ],
   }),
 
   watch: {
